5087df565e64233318ffc05958edfe75d4468a19
Leo Roos <leoroos88@gmail.com>
2026-01-28 15:21:11 +0100

    update MainPage to use topic data from UserPick

---
+++ MainPage/index.js
@@ -0,0 +1,11 @@
+const chosenTopic = localStorage.getItem("chosenTopic")
+
+const chosenTopicEl = document.getElementById("chosenTopic")
+if (chosenTopic) {
+    chosenTopicEl.textContent = chosenTopic
+}
+
+const chosenTopicBtnEl = document.getElementById("changeTopicBtn")
+chosenTopicBtnEl.addEventListener("click", function(event) {
+    location.pathname = "/MainPage/UserPick"
+})
